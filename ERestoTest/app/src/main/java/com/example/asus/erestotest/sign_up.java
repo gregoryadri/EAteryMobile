a2c59840e74f20a8c075bd0110077cec80172820
kusumo <shirokamiyuuto@gmail.com>
2017-11-04 19:52:41 +0700

    signup & few changes to strings.xml

--- ERestoTest/app/src/main/java/com/example/asus/erestotest/sign_up.java
+++ ERestoTest/app/src/main/java/com/example/asus/erestotest/sign_up.java
@@ -6,15 +6,43 @@ import android.support.design.widget.Snackbar;
 import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.Toolbar;
 import android.view.View;
+import android.app.Activity;
+import android.view.View.OnClickListener;
+import android.widget.Button;
+import android.widget.RadioButton;
+import android.widget.RadioGroup;
+import android.widget.RadioGroup.OnCheckedChangeListener;
+import android.widget.TextView;
+import android.widget.Toast;
 
 public class sign_up extends AppCompatActivity {
-
+    private RadioGroup radioGroup;
+    private RadioButton pelanggan, restoran;
+    private TextView textView;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_sign_up);
         Toolbar toolbar = (Toolbar) findViewById(R.id.toolbar);
         setSupportActionBar(toolbar);
+
+        radioGroup = (RadioGroup) findViewById(R.id.radio_signup);
+        radioGroup.setOnCheckedChangeListener(new OnCheckedChangeListener() {
+
+            @Override
+            public void onCheckedChanged(RadioGroup group, int checkedId) {
+                // find which radio button is selected
+                if (checkedId == R.id.radio_pelanggan) {
+                    Toast.makeText(getApplicationContext(), "Pelanggan telah dipilih",
+                            Toast.LENGTH_SHORT).show();
+                }
+                if (checkedId == R.id.radio_restoran) {
+                    Toast.makeText(getApplicationContext(), "Restoran telah dipilih",
+                            Toast.LENGTH_SHORT).show();
+                }
+            }
+        });
+
     }
 
 }
