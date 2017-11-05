e22d51881fd3de94d218d273b4102de92c0ce70a
kusumo <shirokamiyuuto@gmail.com>
2017-11-05 21:37:45 +0700

    checkout page, show order, and some optimization

--- ERestoTest/app/src/main/java/com/example/asus/erestotest/show_order.java
+++ ERestoTest/app/src/main/java/com/example/asus/erestotest/show_order.java
@@ -1,11 +1,23 @@
 package com.example.asus.erestotest;
 
+import android.content.Intent;
+import android.support.design.widget.NavigationView;
+import android.support.v4.view.GravityCompat;
+import android.support.v4.widget.DrawerLayout;
+import android.support.v7.app.ActionBarDrawerToggle;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
+import android.view.Menu;
+import android.view.MenuItem;
+import android.view.View;
+import android.widget.Button;
+import android.widget.ListView;
 
-public class show_order extends AppCompatActivity {
-
+public class show_order extends AppCompatActivity
+        implements NavigationView.OnNavigationItemSelectedListener, View.OnClickListener{
+    Button pembayaran, kembali;
+    ListView list_makanan, list_minuman;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -14,5 +26,77 @@ public class show_order extends AppCompatActivity {
         setSupportActionBar(toolbar);
 
 
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        ActionBarDrawerToggle toggle = new ActionBarDrawerToggle(
+                this, drawer, toolbar, R.string.navigation_drawer_open, R.string.navigation_drawer_close);
+        drawer.setDrawerListener(toggle);
+        toggle.syncState();
+
+        NavigationView navigationView = (NavigationView) findViewById(R.id.nav_view);
+        navigationView.setNavigationItemSelectedListener(this);
+
+        pembayaran = (Button)findViewById(R.id.checkout);
+
+        list_makanan = (ListView)findViewById(R.id.list_makanan);
+        list_minuman = (ListView)findViewById(R.id.list_minuman);
+
+        kembali.setOnClickListener(this);
+        pembayaran.setOnClickListener(this);
+    }
+
+    public void onBackPressed() {
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        if (drawer.isDrawerOpen(GravityCompat.START)) {
+            drawer.closeDrawer(GravityCompat.START);
+        } else {
+            super.onBackPressed();
+        }
+    }
+
+    @Override
+    public boolean onCreateOptionsMenu(Menu menu) {
+        // Inflate the menu; this adds items to the action bar if it is present.
+        getMenuInflater().inflate(R.menu.main, menu);
+        return true;
+    }
+
+    @Override
+    public boolean onOptionsItemSelected(MenuItem item) {
+        // Handle action bar item clicks here. The action bar will
+        // automatically handle clicks on the Home/Up button, so long
+        // as you specify a parent activity in AndroidManifest.xml.
+        int id = item.getItemId();
+
+        //noinspection SimplifiableIfStatement
+        if (id == R.id.action_settings) {
+            return true;
+        }
+
+        return super.onOptionsItemSelected(item);
+    }
+
+    @SuppressWarnings("StatementWithEmptyBody")
+
+
+    public boolean onNavigationItemSelected(MenuItem item) {
+        // Handle navigation view item clicks here.
+        int id = item.getItemId();
+
+        if (id == R.id.menu) {
+            startActivity(new Intent(getApplicationContext(),menu.class));
+        }
+
+        DrawerLayout drawer = (DrawerLayout) findViewById(R.id.drawer_layout);
+        drawer.closeDrawer(GravityCompat.START);
+        return true;
+    }
+
+    public void onClick(View v) {
+        if(v == kembali){
+            startActivity(new Intent(getApplicationContext(),menu.class));
+        }
+        if (v == pembayaran){
+            //startActivity(new Intent(getApplicationContext(),checkout.class));
+        }
     }
 }
